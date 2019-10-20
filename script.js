2c8c18948ce4abb9b8d7cbc561ff247d95cec800
kademanec <kademanec@gmail.com>
2019-10-20 12:45:57 +0530

    bankapp

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-var testApp = angular.module("testApp", []);
+var testApp = angular.module("testApp",  ['ui.bootstrap']);
 
 testApp.controller("testController", function($scope, $http) {
   $scope.home = "This is the homepage";
 
   $scope.names = ["BANGALORE", "DAVANGERE", "PUNE","MUMBAI","HUBLI"];
-
   $scope.getRequest = function() {
     console.log("I've been pressed!");
 
+
     $http.get("https://vast-shore-74260.herokuapp.com/banks?city="+$scope.selectedName).then(
-      function successCallback(response) {
+      function successCallback(response,multi) {
         $scope.response = response;
 
         console.log(response.data,"correctresponse");
+
 
       },
       function errorCallback(response) {
@@ -21,29 +22,5 @@ testApp.controller("testController", function($scope, $http) {
     );
   };
 
-//   $scope.filterNames = function(searchText, searchFilter) {
-//   if (searchFilter === 'bank_name') {
-//     return function (x) { return x.bank_name.match(searchText); }
-//   }
-//   else if (searchFilter === 'ifsc') {
-//     return function (x) { return x.ifsc.match(searchText); }
-//   }
-//   else if (searchFilter === 'branch') {
-//     return function (x) { return x.branch.match(searchText); }
-//   }
-//   else if (searchFilter === 'address') {
-//     return function (x) { return x.address.match(searchText); }
-//   }
-//   else if (searchFilter === 'district') {
-//     return function (x) { return x.district.match(searchText); }
-//   }
-//   else {
-//     return function (x) { return x.bank_name.match(searchText) || x.ifsc.match(searchText) || x.branch.match(searchText) || x.address.match(searchText) || x.district.match(searchText); }
-//   }
-// };
 
-
-
-
-
-});
+ });
